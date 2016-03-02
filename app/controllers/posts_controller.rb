018e7a01d20404f45940238de71708b1f6b0dc62
timlichen <tim.li.chen@gmail.com>
2016-03-02 14:53:15 -0800

    added alerts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -16,8 +16,10 @@ class PostsController < ApplicationController
   # Controller that handle updating logic
   def update
     if @post.update(post_params)
+      flash.now[:alert] = "Post updated successfully!"
       redirect_to(post_path(@post))
     else
+      flash.now[:alert] = "You post couldn't be updated! Check the form and try again!"
       redirect_to posts_path
     end
   end
@@ -31,8 +33,10 @@ class PostsController < ApplicationController
   # Using the data in out form for submission
   def create
     if @post = Post.create(post_params)
+      flash.now[:alert] = "You post has been created successfully."
       redirect_to posts_path
     else
+      flash.now[:alert] = "Your new post couldn't be created. Check the form and try again."
       render :new
     end
   end
