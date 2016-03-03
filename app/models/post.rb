e14c3f9ff597084c8c9da1e85edd8600bf597fce
timlichen <tim.li.chen@gmail.com>
2016-03-03 12:46:38 -0800

    added comments and AJAX

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  has_many :comments, dependent: :destroy
   validates :image, presence: true
   validates :user_id, presence: true
+  validates :caption, length: {minimum: 3, maximum: 300}
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
